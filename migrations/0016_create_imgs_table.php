afdc9101397024fb333f062a72f1267be5e5d3b1
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-06-27 13:12:31 +0300

    solving images table

--- migrations/0016_create_imgs_table.php
+++ migrations/0016_create_imgs_table.php
@@ -7,7 +7,7 @@ $query = "CREATE TABLE images(
           userId INT(11) NOT NULL,
           moviesId INT(11) NOT NULL,
           path varchar(255) not null ,
-          FOREIGN KEY (moviesId) REFERENCES movies(id) 
+          FOREIGN KEY (moviesId) REFERENCES movies(id) ,
           FOREIGN KEY (userId) REFERENCES users(id)
 )";
 $execute = $mysqli->prepare($query);
