df1e089c026a1db19c89f751a01c79f2c6079b13
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-06-27 11:44:43 +0300

    finishing the   model

--- migrations/0014_create_notifcations_table.php
+++ migrations/0014_create_notifcations_table.php
@@ -4,7 +4,7 @@ require("../connection/connection.php");
 
 $query = "CREATE TABLE notifcations(
           id INT(11) AUTO_INCREMENT PRIMARY KEY, 
-        
+         mess VARCHAR(255) NOT NULL,
           userId INT(11) NOT NULL,
           FOREIGN KEY (userId) REFERENCES users(id)
           )";
--- migrations/008_create_payments_table.php
+++ migrations/008_create_payments_table.php
@@ -5,6 +5,7 @@ require("../connection/connection.php");
 $query = "CREATE TABLE payments(
           id INT(11) AUTO_INCREMENT PRIMARY KEY, 
           typename VARCHAR(255) NOT NULL, 
+          userId INT(11) NOT NULL,
           FOREIGN KEY (userId) REFERENCES users(id)
           )";
 
