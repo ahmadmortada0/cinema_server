3a565c85434b6781b9ae8c2eadef2288f8d0ab9d
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-06-27 12:11:46 +0300

    changing some logic

--- migrations/0010_create_geners_table.php
+++ migrations/0010_create_geners_table.php
@@ -5,7 +5,6 @@ require("../connection/connection.php");
 $query = "CREATE TABLE genres(
           id INT(11) AUTO_INCREMENT PRIMARY KEY, 
           genretype VARCHAR(255) NOT NULL, 
-          FOREIGN KEY (userId) REFERENCES users(id)
           )";
 
 $execute = $mysqli->prepare($query);
