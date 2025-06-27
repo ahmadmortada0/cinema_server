2ac299c30fd70b16cea7bde404e9f3ca74b5ba17
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-06-27 11:42:44 +0300

    finishing the Snack  model

--- models/Snack.php
+++ models/Snack.php
@@ -1,43 +1,41 @@
 <?php
 require_once("Model.php");
 
-class Snack extends Model{
+class Snack extends Model {
 
-    private int $id; 
-    private int $price; 
-    private string $name; 
-    
+    private int $id;
+    private string $name;
+    private int $price;
+
     protected static string $table = "snacks";
 
-    public function __construct(array $data){
+    public function __construct(array $data) {
         $this->id = $data["id"];
-        $this->price = $data["price"];
         $this->name = $data["name"];
+        $this->price = $data["price"];
     }
 
     public function getId(): int {
         return $this->id;
     }
 
-    public function getprice(): int {
-        return $this->price;
-    }
-
-
-    public function getname(): string {
+    public function getName(): string {
         return $this->name;
     }
 
-    public function setprice(int $price){
-        $this->price = $price;
+    public function getPrice(): int {
+        return $this->price;
     }
-    public function name(string $name){
+
+    public function setName(string $name) {
         $this->name = $name;
     }
 
-
-    public function toArray(){
-        return [$this->id, $this->price,  $this->name];
+    public function setPrice(int $price) {
+        $this->price = $price;
     }
-    
+
+    public function toArray() {
+        return [$this->id, $this->name, $this->price];
+    }
 }
